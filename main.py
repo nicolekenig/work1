205efb80c54ff3956a7ce308b3a6fef739999560
nicolekenig <49375560+nicolekenig@users.noreply.github.com>
2022-04-27 12:07:12 +0300

    works and fixed!

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import time
+
 import pandas as pd
 from dataSystem import dataSystem
 from diagnosis import diagnosis
@@ -49,29 +51,30 @@ def read_data_observation_files(name_prefix):
                 ans = systems[sys_id].compare_output_to_obs_output()
                 print("Observation num ", obs_num, " - ", ans)
                 diagnos.set_obs_dict(obs_num, ans)
+                start_time = time.time()
                 diagnosis_ans = diagnos.run_bfs(systems[sys_id], systems[sys_id].get_sys_comps_dict())
-
+                end_time = time.time()
                 # without timeout
-                # ans_dict['System Name'][line_num] = sys_id
-                # ans_dict['Observation no.'][line_num] = obs_num
-                # ans_dict['Number of Diagnoses'][line_num] = len(diagnosis_ans)
-                # ans_dict[ 'Minimal Cardinality'][line_num] =  len(diagnosis_ans[0])
-                # ans_dict['Maximal Cardinality'][line_num] = '--'
-                # ans_dict['Runtim e (ms)'][line_num] = end_time-start_time
-                # line_num += 1
-                # with timeout
                 ans_dict['System Name'][line_num] = sys_id
                 ans_dict['Observation no.'][line_num] = obs_num
                 ans_dict['Number of Diagnoses'][line_num] = len(diagnosis_ans)
-                ans_dict['Minimal Cardinality'][line_num] = len(diagnosis_ans[0])
-                ans_dict['Maximal Cardinality'][line_num] = len(diagnosis_ans[-1])
-                ans_dict['Runtim e (ms)'][line_num] = '--'
+                ans_dict[ 'Minimal Cardinality'][line_num] =  len(diagnosis_ans[0])
+                ans_dict['Maximal Cardinality'][line_num] = '--'
+                ans_dict['Runtim e (ms)'][line_num] = end_time-start_time
                 line_num += 1
+                # with timeout
+                # ans_dict['System Name'][line_num] = sys_id
+                # ans_dict['Observation no.'][line_num] = obs_num
+                # ans_dict['Number of Diagnoses'][line_num] = len(diagnosis_ans)
+                # ans_dict['Minimal Cardinality'][line_num] = len(diagnosis_ans[0])
+                # ans_dict['Maximal Cardinality'][line_num] = len(diagnosis_ans[-1])
+                # ans_dict['Runtim e (ms)'][line_num] = '--'
+                # line_num += 1
                 print("diagnosis_ans: ", diagnosis_ans)
                 print()
             # diagnos.print_obs()
     data = pd.DataFrame(ans_dict)
-    data.to_csv("output_74283.csv")
+    data.to_csv("output_c17.csv")
 
 def convert_string_to_arr(s):
     s = s.replace('[', '')
@@ -88,10 +91,10 @@ if __name__ == '__main__':
     # lst = ["a", "b", "c"]
     # ans = list(combinations(lst, 2))
     # print(ans)
-    # print("*********** system c17 ***********")
-    # read_data_system_files('c17')
-    # read_data_observation_files('c17')
-    # print()
+    print("*********** system c17 ***********")
+    read_data_system_files('c17')
+    read_data_observation_files('c17')
+    print()
     # print("*********** system 74181 ***********")
     # read_data_system_files('74181')
     # p1 = Process(target=read_data_observation_files('74181'))
@@ -103,7 +106,7 @@ if __name__ == '__main__':
     # read_data_system_files('74182')
     # read_data_observation_files('74182')
     # print()
-    print("*********** system 74283 ***********")
-    read_data_system_files('74283')
-    read_data_observation_files('74283')
-    print()
+    # print("*********** system 74283 ***********")
+    # read_data_system_files('74283')
+    # read_data_observation_files('74283')
+    # print()
